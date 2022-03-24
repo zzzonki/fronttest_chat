db8958c164832cb759f9a97e9266dac643bec98e
ivanchenko.k <ivanchenko.k@rdn-grp.ru>
2022-03-24 17:02:08 +0300

    code style fixes

--- gulpfile.js
+++ gulpfile.js
@@ -3,47 +3,52 @@ var sass = require('gulp-sass')(require('sass'));
 var svgSprite = require('gulp-svg-sprite');
 var clean = require('gulp-clean');
 
-gulp.task('sсss', function (done) {
+gulp.task('scss', function (done) {
     return gulp
-        .src(['src/sсss/style.scss'])
+        .src(['src/scss/style.scss'])
         .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
         .pipe(gulp.dest('build/css'));
 
     done();
 });
 
-gulp.task('js', function(){
-    return gulp.src(['src/js/*.js']).pipe(gulp.dest('build/js'))
-})
+gulp.task('js', function () {
+    return gulp.src(['src/js/*.js']).pipe(gulp.dest('build/js'));
+});
 
-gulp.task('html', function(){
-    return gulp.src(['src/index.html']).pipe(gulp.dest('build'))
-})
+gulp.task('html', function () {
+    return gulp.src(['src/index.html']).pipe(gulp.dest('build'));
+});
 
 gulp.task('svgSprite', function () {
-    return gulp.src('src/icons/*.svg') // svg files for sprite
-        .pipe(svgSprite({
+    return gulp
+        .src('src/icons/*.svg') // svg files for sprite
+        .pipe(
+            svgSprite({
                 mode: {
-                    symbol:{
-                        sprite: "../sprite.svg"  //sprite file name
-                    }
+                    symbol: {
+                        sprite: '../sprite.svg', //sprite file name
+                    },
                 },
-            }
-        ))
+            })
+        )
         .pipe(gulp.dest('build/icons'));
 });
 
-gulp.task('clean', function(){
+gulp.task('clean', function () {
     return gulp.src(['build/*']).pipe(clean());
-})
+});
 
 gulp.task('watch', function () {
-    gulp.watch(['src/sсss/**/*.scss', ''], gulp.series('sсss'));
+    gulp.watch(['src/scss/**/*.scss', ''], gulp.series('scss'));
     gulp.watch(['src/js/**/*.js', ''], gulp.series('js'));
     gulp.watch(['src/icons/**/*.svg', ''], gulp.series('svgSprite'));
     gulp.watch(['src/index.html', ''], gulp.series('html'));
 });
 
-gulp.task('build', gulp.series('clean', gulp.parallel('html', 'sсss', 'js', 'svgSprite')));
+gulp.task(
+    'build',
+    gulp.series('clean', gulp.parallel('html', 'scss', 'js', 'svgSprite'))
+);
 
 gulp.task('default', gulp.series('build', 'watch'));
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,14 @@
-let chatbox = document.querySelector('.chat-box');
-input = document.querySelector('.input__text');
-form = document.querySelector('.mes-form');
-emoji = document.querySelector('.input__emoji');
-emoji_field = document.querySelector('.emoji-list');
-select = document.querySelector('.select');
-select_header = document.querySelector('.select__header');
-select_items = document.querySelectorAll('.select__item');
-select_current = document.querySelector('.select__current');
-select_content = document.querySelector('.select__content');
-input_img = document.querySelector('#input-img');
+let chatbox = document.querySelector('.chat-box'),
+    input = document.querySelector('.input__text'),
+    form = document.querySelector('.mes-form'),
+    emoji = document.querySelector('.input__emoji'),
+    emoji_field = document.querySelector('.emoji-list'),
+    select = document.querySelector('.select'),
+    select_header = document.querySelector('.select__header'),
+    select_items = document.querySelectorAll('.select__item'),
+    select_current = document.querySelector('.select__current'),
+    select_content = document.querySelector('.select__content'),
+    input_img = document.querySelector('#input-img');
 
 let messages = [];
 
@@ -16,24 +16,30 @@ let flag = true;
 
 select_header.addEventListener('click', () => {
     select_content.classList.add('animated');
+
     if (flag) {
         select_content.classList.remove('fade-out-up');
         select_content.classList.add('fade-in-down');
         select.classList.toggle('active');
+
         flag = false;
     } else {
         select_content.classList.remove('fade-in-down');
         select_content.classList.add('fade-out-up');
-        flag = true;
         select.classList.remove('active');
+
+        flag = true;
     }
 });
+
 select_items.forEach((item) => {
     item.addEventListener('click', () => {
         select_content.classList.remove('fade-in-down');
         select_content.classList.add('fade-out-up');
-        flag = true;
         select.classList.remove('active');
+
+        flag = true;
+
         select_current.innerText = item.innerText;
     });
 });
@@ -41,10 +47,12 @@ select_items.forEach((item) => {
 let socket = new WebSocket(
     'wss://demo.piesocket.com/v3/channel_1?api_key=oCdCMcMPQpbvNjUIzqtvF1d2X2okWpDQj4AwARJuAgtjhzKxVEjQU6IdCjwm&notify_self'
 );
+
 socket.onopen = function (event) {
     console.log('opened');
     socket.send('opened');
 };
+
 socket.onmessage = (event) => {
     let message = {
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam odio quam blanditiis natus, deserunt voluptates, harum provident laboriosam nihil autem eum, pariatur nemo nostrum! Doloremque expedita quis non facere voluptate.',
@@ -53,16 +61,22 @@ socket.onmessage = (event) => {
     };
     sendMessage(message);
 };
+
 socket.onclose = function (event) {
     console.log('closed');
 };
+
 input_img.addEventListener('change', (event) => {
     event.preventDefault();
+    
     let photo = event.target.files[0];
+
     if (!photo.type.match('image.*')) {
         return;
     }
+
     let FR = new FileReader();
+
     FR.onload = function (event) {
         chatbox.insertAdjacentHTML(
             'beforeEnd',
@@ -75,11 +89,13 @@ input_img.addEventListener('change', (event) => {
             block: 'end',
         });
     };
+
     FR.readAsDataURL(photo);
 });
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
+
     if (input.value) {
         let message = {
             text: input.value,
